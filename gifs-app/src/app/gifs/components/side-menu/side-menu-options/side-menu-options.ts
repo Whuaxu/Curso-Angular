c649a42f7180c08d94cb3f4953e4ee1b51d334b3
javier.pena <javierpena@alialabs.com>
2025-11-27 14:19:06 +0100

    Encapsulated code of options of the menu

--- gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface menuOption {
+  label: string;
+  subLabel: string;
+  router: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html'
 })
-export class SideMenuOptions { }
+export class SideMenuOptions { 
+
+
+  menuOptions: menuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      router: 'dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      router: 'dashboard/search',
+    },
+  ];
+}
+
