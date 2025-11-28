69f537ca85db19eee4eea42903ec11158fe7f169
javier.pena <javierpb6@gmail.com>
2025-11-28 11:58:20 +0100

    Added search structure to receive querys and treat them

--- gifs-app/src/app/gifs/pages/search-page/search-page.ts
+++ gifs-app/src/app/gifs/pages/search-page/search-page.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GifList } from '../../components/gif-list/gif-list';
+import { GifsService } from '../../services/gifs';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPage { }
+export default class SearchPage { 
+
+  gifService = inject(GifsService);
+
+  onSearch(query: string){
+    this.gifService.searchGifs(query);
+  }
+}
