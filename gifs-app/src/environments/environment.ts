a64627a208ba5ea45adfc1cf9207aef330dede7d
javier.pena <javierpena@alialabs.com>
2025-11-28 09:37:46 +0100

    Added environments

---
+++ gifs-app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    compannySlogan: 'Find the best gifs here!',
+
+    //ApiKeys
+
+    //URLS
+
+};
